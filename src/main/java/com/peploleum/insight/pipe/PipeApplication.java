6ad8472daa479f65ce18e4bad262f454a7f60070
peploleum <peploleum@gmail.com>
2018-09-27 07:27:53 +0200

    moved properties to yml

--- src/main/java/com/peploleum/insight/pipe/PipeApplication.java
+++ src/main/java/com/peploleum/insight/pipe/PipeApplication.java
@@ -2,11 +2,21 @@ package com.peploleum.insight.pipe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
 
 @SpringBootApplication
+@EnableBinding(Sink.class)
 public class PipeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PipeApplication.class, args);
+
+    }
+
+    @StreamListener(Sink.INPUT)
+    public void handle(String message) {
+        System.out.println("Received: " + message);
     }
 }
